d1a947fd78dcb1d0706922eef43a4d50e2d65ac2
xi816 <jej07267@gmail.com>
2025-05-23 14:20:52 +0300

    Маллушка

--- mall.c
+++ mall.c
@@ -2,7 +2,8 @@
 #include <stdlib.h>
 
 int main() {
-	system("clang -o kernel.elf kernel/main.c kernel/init_kernel.c kernel/panic.c kernel/shell.c kernel/vfs.c");
-	system("clang -o tinyboot boot/tinyboot.c");
-	return 0;
+  system("gcc -o kernel.elf kernel/main.c kernel/init_kernel.c kernel/panic.c kernel/shell.c kernel/vfs.c");
+  system("gcc -o tinyboot boot/tinyboot.c");
+  system("gcc -o create-disk create-disk.c");
+  return 0;
 }
