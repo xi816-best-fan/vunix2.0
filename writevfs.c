ca9e7ac963e4d2cf09b4efef3ef48a454340a8a3
xi816 <jej07267@gmail.com>
2025-05-30 11:57:33 +0300

    shit

--- writevfs.c
+++ writevfs.c
@@ -2,10 +2,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
-#include <string.h>
 #include <unistd.h>
+#include <string.h>
 
-#define MAX_DISK_SIZE 10240 // десот келобайт
+#define MAX_DISK_SIZE 16384 // много келобайт
+#define MAX_FILE_SIZE 8192  // восем келобайт
 #define MAGIC      0xDEADBABE
 #define DISK_END   0xFF
 #define FILE_START 0xFE
@@ -45,13 +46,18 @@ uint32_t ins_str(char* buf, char* str, uint32_t offset) {
   return offset+len+1;
 }
 
-uint32_t write_file(char* buf, char* filename, char* path, char* contents, uint32_t offset) {
+uint32_t ins_membuf(char* buf, char* str, uint32_t size, uint32_t offset) {
+  memcpy(buf+offset, str, size);
+  return offset+size;
+}
+
+uint32_t write_file(char* buf, char* filename, char* path, char* contents, uint32_t csize, uint32_t offset) {
   offset = ins_U8(buf, FILE_START, offset);
   offset = ins_str(buf, filename, offset);
   offset = ins_U8(buf, FILE_META, offset);
   offset = ins_str(buf, path, offset);
   offset = ins_U8(buf, FILE_META, offset);
-  offset = ins_str(buf, contents, offset)-1;
+  offset = ins_membuf(buf, contents, csize, offset);
   offset = ins_U8(buf, FILE_START, offset);
   return offset;
 }
@@ -65,28 +71,55 @@ uint32_t write_dir(char* buf, char* name, char* path, uint32_t offset) {
   return offset;
 }
 
-int32_t main(void) {
-  srand(time(NULL));
-  FILE* f = fopen("disk.img", "r+b");
-  if (!f) {
-    fprintf(stderr, "ты чё даун нет диска\n");
-    return 0;
+int main(int argc, char** argv) {
+  puts("writevfs 1.0");
+  if (argc < 5) {
+    printf("Usage: %s disk.img file.bin file /dir\n", argv[0]);
+    return 1;
   }
-  char buf[MAX_DISK_SIZE];
-  uint32_t offs = 0;
-  uint64_t bits = rand();
-  bits <<= 31;
-  bits |= rand();
-  offs = ins_U32(buf, MAGIC, offs);
-  offs = ins_U64(buf, bits, offs);
+  char* diskname     = argv[1];
+  char* filename     = argv[2];
+  char* new_filename = argv[3];
+  char* directory    = argv[4];
+  printf("  Disk name:     %s\n", diskname);
+  printf("  File name:     %s\n", filename);
+  printf("  New file name: %s\n", new_filename);
+  printf("  Directory:     %s\n", directory);
 
-  offs = write_file(buf, "hello.txt", "/hello", "Hello World!", offs);
-  offs = write_dir(buf, "hello", "/hello", offs);
-  offs = ins_U8(buf, DISK_END, offs);
-
-  fseek(f, 0, SEEK_SET);
-  fwrite(buf, 1, MAX_DISK_SIZE, f);
+  // File
+  FILE* f = fopen(filename, "rb");
+  if (!f) {
+    fprintf(stderr, "error: file `%s` does not exist\n", filename);
+    return 1;
+  }
+  char contents[MAX_FILE_SIZE];
+  fread(contents, 1, MAX_FILE_SIZE, f);
   fclose(f);
+
+  // Disk
+  FILE* f2 = fopen(diskname, "r+b");
+  if (!f2) {
+    fprintf(stderr, "error: disk `%s` does not exist\n", diskname);
+    return 1;
+  }
+  unsigned char buf[MAX_DISK_SIZE];
+  fread(buf, 1, MAX_DISK_SIZE, f2);
+  fseek(f2, 0, SEEK_END);
+  uint32_t csize = ftell(f2);
+  uint32_t offs = 0;
+  while (buf[offs] != 0xFF) offs++;
+
+  buf[offs] = 0x00;
+  offs = write_file(buf, new_filename, directory, contents, csize, offs);
+  offs = ins_U8(buf, 0xFF, offs);
+  for (int i = 0x3C; i < 0x80; i++) {
+    printf("%02X ", buf[i]);
+  }
+  puts("\b");
+
+  fseek(f2, 0, SEEK_SET);
+  fwrite(buf, 1, MAX_DISK_SIZE, f2);
+  fclose(f2);
   return 0;
 }
 
